308225edf417220dd2c75e62d005d46fb8ee5c20
shauryagupta45 <shauryagupta4501@gmail.com>
2023-10-19 20:29:33 +0530

    theme changer added

---
+++ lib/Screens/theme_changer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThemeChanger extends StatefulWidget {
+  const ThemeChanger({Key? key}) : super(key: key);
+
+  @override
+  State<ThemeChanger> createState() => _ThemeChangerState();
+}
+
+class _ThemeChangerState extends State<ThemeChanger> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:provider/provider.dart';
 import 'package:provider_basic/Provider/count_provider.dart';
 import 'package:provider_basic/Provider/favorite_item_provider.dart';
 import 'package:provider_basic/Provider/sliderScreenProvider.dart';
+import 'package:provider_basic/Provider/theme_provider.dart';
 import 'package:provider_basic/Screens/count_screen.dart';
 import 'package:provider_basic/Screens/favorite_screen.dart';
 import 'package:provider_basic/Screens/sliderScreen.dart';
+import 'package:provider_basic/Screens/theme_changer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +24,7 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider(create: (_) => CountProvider()),
           ChangeNotifierProvider(create: (_) => SliderProvider()),
           ChangeNotifierProvider(create: (_) => FavoriteItemProvider()),
+          ChangeNotifierProvider(create: (_) => ThemeProvider()),
         ],
         child: MaterialApp(
           title: 'Flutter Demo',
@@ -29,7 +32,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          home: const FavoriteApp(),
+          home: const ThemeChanger(),
         ));
   }
 }
