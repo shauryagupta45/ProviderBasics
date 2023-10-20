439e5c196efa079bdf56666ebc78454855b240ee
shauryagupta45 <shauryagupta4501@gmail.com>
2023-10-20 23:59:27 +0530

    hello

---
+++ lib/Screens/stless_as_stful.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NotifyListenerScreen extends StatelessWidget {
+  const NotifyListenerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Statless as Stateful"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Counter Screen"),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: onPressed),
+    );
+  }
+}
