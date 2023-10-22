def6430ef01c996c3c5f219bd20a3b1e608542e0
shauryagupta45 <shauryagupta4501@gmail.com>
2023-10-23 00:35:14 +0530

    updated code

--- lib/Screens/stless_as_stful.dart
+++ lib/Screens/stless_as_stful.dart
@@ -4,6 +4,9 @@ class NotifyListenerScreen extends StatelessWidget {
   NotifyListenerScreen({Key? key}) : super(key: key);
 
   ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  ValueNotifier<bool> _toggle = ValueNotifier<bool>(true);
+
+  // disadvantage of all this is, we can't dispose these notifiers
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,15 +14,36 @@ class NotifyListenerScreen extends StatelessWidget {
         title: Text("Statless as Stateful"),
         centerTitle: true,
       ),
-      body: Center(
-          child: ValueListenableBuilder(
-              valueListenable: _counter,
+      body: Column(
+        children: [
+          ValueListenableBuilder(
+              valueListenable: _toggle,
               builder: (context, value, child) {
-                return Text(
-                  _counter.value.toString(),
-                  style: const TextStyle(fontSize: 50),
+                return TextFormField(
+                  obscureText: _toggle.value,
+                  decoration: InputDecoration(
+                    suffix: InkWell(
+                      child: Icon(
+                        _toggle.value ? Icons.visibility_off : Icons.visibility,
+                      ),
+                      onTap: () {
+                        _toggle.value = !_toggle.value;
+                      },
+                    ),
+                  ),
                 );
-              })),
+              }),
+          Center(
+              child: ValueListenableBuilder(
+                  valueListenable: _counter,
+                  builder: (context, value, child) {
+                    return Text(
+                      _counter.value.toString(),
+                      style: const TextStyle(fontSize: 50),
+                    );
+                  })),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _counter.value++;
