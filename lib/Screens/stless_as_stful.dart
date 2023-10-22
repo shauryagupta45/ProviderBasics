63f1b573d497af01c87542e81768c2cee302ea74
shauryagupta45 <shauryagupta4501@gmail.com>
2023-10-22 23:57:03 +0530

    updated code

--- lib/Screens/stless_as_stful.dart
+++ lib/Screens/stless_as_stful.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class NotifyListenerScreen extends StatelessWidget {
-  const NotifyListenerScreen({Key? key}) : super(key: key);
+  NotifyListenerScreen({Key? key}) : super(key: key);
 
+  ValueNotifier<int> _counter = ValueNotifier<int>(0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,9 +12,21 @@ class NotifyListenerScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(" Counter Screen "),
+          child: ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (context, value, child) {
+                return Text(
+                  _counter.value.toString(),
+                  style: const TextStyle(fontSize: 50),
+                );
+              })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _counter.value++;
+          print(_counter.value.toString());
+        },
+        child: const Icon(Icons.add),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: onPressed),
     );
   }
 }
