c9049fcb6dbbc670988feedf5b9310f7317c6d86
shauryagupta45 <shauryagupta4501@gmail.com>
2023-10-26 23:59:29 +0530

    update

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_basic/Provider/auth_provider.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,26 +14,57 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passController = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Login Screen"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TextFormField(
-            controller: emailController,
-            decoration: InputDecoration(
-              hintText: "Email",
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: "Email",
+              ),
             ),
-          ),
-          TextFormField(
-            controller: passController,
-            decoration: InputDecoration(hintText: "Password"),
-          ),
-        ],
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: passController,
+              obscureText: true,
+              decoration: const InputDecoration(hintText: "Password"),
+            ),
+            const SizedBox(height: 50),
+            InkWell(
+              onTap: () {
+                authProvider.login(emailController.text.toString(),
+                    passController.text.toString());
+              },
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: authProvider.loading
+                      ? CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : Text("Login"),
+
+                  //Text email and pass below :
+                  // "email": "eve.holt@reqres.in",
+                  // "password": "cityslicka"
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
